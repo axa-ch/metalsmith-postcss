0ceb87b4d77870effc5bc37d9fb4d0998e21ce56
David Knezić <davidknezic@gmail.com>
2015-10-01 18:44:52 +0200

    Add basic sourcemap support for inline and external maps

--- index.js
+++ index.js
@@ -3,20 +3,48 @@ var minimatch = require('minimatch');
 
 module.exports = main;
 
-function main(plugins) {
+function main(plugins, options) {
   var plugins = plugins || [];
+  var options = options || {};
+  var map = normalizeMapOptions(options.map);
 
-  var processor = postcss();
-
-  plugins.forEach(function(plugin) {
-    processor.use(plugin);
-  });
+  var processor = postcss(plugins);
 
   return function (files, metalsmith, done) {
     var styles = Object.keys(files).filter(minimatch.filter("*.css", { matchBase: true }));
-    styles.forEach(function (file, index, arr) {
-      files[file].contents = new Buffer(processor.process(files[file].contents.toString()).css);
+
+    styles.forEach(function (file) {
+      var contents = files[file].contents.toString();
+
+      processor
+        .process(contents, {
+          from: file,
+          to: file,
+          map: map
+        })
+        .then(function (result) {
+          files[file].contents = new Buffer(result.css);
+
+           if (result.map) {
+             files[file + '.map'] = {
+               contents: new Buffer(JSON.stringify(result.map)),
+               mode: files[file].mode,
+               stats: files[file].stats
+             };
+           }
+        });
     });
     done();
   }
+}
+
+function normalizeMapOptions(map) {
+  if (!map) return undefined;
+
+  return {
+    inline: map === true ? true : map.inline,
+    prev: false, // Not implemented yet
+    sourcesContent: undefined,  // Not implemented yet
+    annotation: undefined // Not implemented yet
+  };
 }
--- test/index.js
+++ test/index.js
@@ -20,5 +20,33 @@ describe('metalsmith-postcss', function () {
           done();
         });
     });
+
+    it('should add inline sourcemaps', function (done) {
+      var metalsmith = Metalsmith(fixture('inline-sourcemaps'));
+      metalsmith
+        .use(postcss([], {
+          map: true
+        }))
+        .build(function (err) {
+          if (err) return done(err);
+          equal(fixture('inline-sourcemaps/build'), fixture('inline-sourcemaps/expected'));
+          done();
+        });
+    });
+
+    it('should add external sourcemap files', function (done) {
+      var metalsmith = Metalsmith(fixture('external-sourcemaps'));
+      metalsmith
+        .use(postcss([], {
+          map: {
+            inline: false
+          }
+        }))
+        .build(function (err) {
+          if (err) return done(err);
+          equal(fixture('external-sourcemaps/build'), fixture('external-sourcemaps/expected'));
+          done();
+        });
+    });
   });
 });
